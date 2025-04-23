d62bec7b6a8e846e5a3fabaab2cb790899dcb715
thibault-de-boutray <155768989+thibault-de-boutray@users.noreply.github.com>
2025-04-23 21:14:31 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,42 +4,16 @@
 #include <string.h>
 #include <ctype.h>
 
-void libererLexique(t_mot *liste) {
-    t_mot *tmp;
-    while (liste != NULL) {
-        tmp = liste;
-        liste = liste->suivant;
-        free(tmp->mot);
-        free(tmp);
-    }
-}
-
-char *nettoyerMot(char *mot) {
-    int i = 0, j = 0;
-    while (mot[i] != '\0') {
-        if (isalpha((unsigned char)mot[i])) {
-            mot[j++] = mot[i];
-        }
-        i++;
-    }
-    mot[j] = '\0';
-    return mot;
-}
-
-int choisirLexique() {
-    int choix;
-    do {
-        printf("\nChoisissez le lexique (1 ou 2) : ");
-        scanf("%d", &choix);
-        viderBuffer();
-    } while (choix != 1 && choix != 2);
-    return choix;
-}
+#define NB_LEXIQUES 5  
 
 int main() {
     char choixMenu = '0';
-    t_mot *lexique1 = NULL;
-    t_mot *lexique2 = NULL;
+    t_mot **lexiques = calloc(NB_LEXIQUES, sizeof(t_mot*)); 
+
+    if (lexiques == NULL) {
+        fprintf(stderr, "Erreur d'allocation mémoire\n");
+        return 1;
+    }
 
     do {
         printf("\n========================================");
@@ -55,50 +29,58 @@ int main() {
         viderBuffer();
 
         char mot[256];
-        t_mot **lexique = NULL;
+        int index;
 
         switch (choixMenu) {
             case '1':
-                lexique = (choisirLexique() == 1) ? &lexique1 : &lexique2;
-                printf("\n\n--- Lexique ---\n");
-                afficherMots(*lexique);
+                index = choisirLexique(NB_LEXIQUES);
+                printf("\n--- Lexique %d : affichage en cours ---\n", index + 1);
+                afficherMots(lexiques[index]);
+                printf("Affichage terminé.\n");
                 break;
 
             case '2':
-                lexique = (choisirLexique() == 1) ? &lexique1 : &lexique2;
-                do {
-                    printf("\nEntrez le mot à ajouter : ");
-                    if (fgets(mot, sizeof(mot), stdin) == NULL) {
-                        mot[0] = '\0';
-                    } else {
-                        mot[strcspn(mot, "\n")] = '\0';
-                    }
-                    nettoyerMot(mot);
-                } while (strlen(mot) == 0);
-                *lexique = ajouterMot(*lexique, convertir_minuscule(mot));
+                index = choisirLexique(NB_LEXIQUES);
+                printf("\nEntrez le mot à ajouter : ");
+                if (fgets(mot, sizeof(mot), stdin) != NULL) {
+                    mot[strcspn(mot, "\n")] = '\0';
+                    lexiques[index] = ajouterMot(lexiques[index], mot);
+                    printf("Mot '%s' ajouté au lexique %d.\n", mot, index + 1);
+                }
                 break;
 
             case '3':
-                lexique = (choisirLexique() == 1) ? &lexique1 : &lexique2;
+                index = choisirLexique(NB_LEXIQUES);
                 printf("\nEntrez le mot à retirer : ");
                 scanf("%255s", mot);
                 viderBuffer();
-                *lexique = retirerMot(*lexique, convertir_minuscule(mot));
+                lexiques[index] = retirerMot(lexiques[index], mot);
+                printf("Mot '%s' retiré du lexique %d.\n", mot, index + 1);
                 break;
 
-            case '4':
-                printf("\nFusion des lexiques en cours...\n");
-                lexique1 = fusionner(lexique1, lexique2);
-                lexique2 = NULL;
+            case '4': {
+                int idx1 = choisirLexique(NB_LEXIQUES);
+                int idx2 = choisirLexique(NB_LEXIQUES);
+                if (idx1 != idx2) {
+                    printf("\nFusion des lexiques %d et %d...\n", idx1 + 1, idx2 + 1);
+                    lexiques[idx1] = fusionner(lexiques[idx1], lexiques[idx2]);
+                    lexiques[idx2] = NULL;
+                    printf("Fusion terminée : lexique %d contient maintenant la fusion.\n", idx1 + 1);
+                } else {
+                    printf("\nVous avez choisi le même lexique pour la fusion. Aucune action effectuée.\n");
+                }
                 break;
+            }
 
             case '5':
-                lexique = (choisirLexique() == 1) ? &lexique1 : &lexique2;
-                *lexique = importerFichier(*lexique);
+                index = choisirLexique(NB_LEXIQUES);
+                printf("\nImportation d'un fichier dans le lexique %d...\n", index + 1);
+                lexiques[index] = importerFichier(lexiques[index]);
+                printf("Importation terminée pour le lexique %d.\n", index + 1);
                 break;
 
             case '6':
-                printf("\n\n*** FIN DU PROGRAMME ***\n");
+                printf("\n*** FIN DU PROGRAMME ***\n");
                 break;
 
             default:
@@ -108,8 +90,7 @@ int main() {
 
     } while (choixMenu != '6');
 
-    libererLexique(lexique1);
-    libererLexique(lexique2);
-
+    libererTousLexiques(lexiques, NB_LEXIQUES);
+    printf("Toutes les ressources ont été libérées.\n");
     return 0;
 }
